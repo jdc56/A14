ab5a3c4a2d338540d59836a942989e81b0c7e538
jdc56 <56010160+jdc56@users.noreply.github.com>
2019-12-03 23:39:37 -0500

    Add files via upload

---
+++ extra/cookie.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+var uInput = document.getElementById('TextArea').value;
+
+function setCookie(){
+	document.cookie = "newCookie=Cookie Saved is: "+uInput;
+	
+}
+
+function getCookie() {
+	  var name =  "newCookie=";
+	  var ca = document.cookie.split(';');
+	  for(var i = 0; i < ca.length; i++) {
+	    var c = ca[i];
+	    while (c.charAt(0) == ' ') {
+	      c = c.substring(1);
+	    }
+	    if (c.indexOf(name) == 0) {
+	      return c.substring(name.length, c.length);
+	    }
+	  }
+	  return "";
+	}
+
+function showCookie(){
+	var cook = getCookie();
+	if(cook != ""){
+		alert("Cookie saved: "+cook);
+	}
+	else{
+		alert("No current cookies.")
+	}
+}
+
